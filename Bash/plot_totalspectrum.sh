06d700d22413d745a71f07d0f6fcdcfa4c76e965
Peter Jansson <peter.jansson@me.com>
2020-09-27 12:08:15 +0200

    Added a script and C++ program to plot the total spectrum from a file from the data archive.

---
+++ Bash/plot_totalspectrum.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Script to plot, in SVG format, a total spectrum of counts
+# out of the file from the data archive gives as $1.
+# The spectrum is written to file given as $2.
+# Uses the C++ program ../Cpp/total_spectrum_from_stdin and curl.
+
+F=spectrum.json
+
+echo -n "[" > "${F}"
+cat "$1" | ../Cpp/total_spectrum_from_stdin >> "${F}"
+echo "]" >> "${F}"
+
+curl -F "datafile=@${F}" \
+     -F "plottype=spectrum" \
+     -F "format=svg" \
+     -F "xlabel=Channel"  \
+     -F "ylabel=Count" \
+     -F "yscale=log10" \
+     https://jansson.net/apps/plot/ -o "$2"
---
+++ Cpp/total_spectrum_from_stdin.cpp
@@ -0,0 +1,40 @@
+#include <cstdint>
+#include <cstdlib>
+#include <iostream>
+#include <map>
+
+#include "clab2019gscans.hpp"
+
+// Parse standard input as one of the files in the data archive and build
+// a spectrum of all counts.
+// Print the spectrum as an array in JSON format on standard output.
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+
+	// channel => count
+	std::map< uint32_t, uint64_t > spectrum;
+
+	// Parse standard input and add all events to the spectrum.
+	for( auto mBatch : clab2019gscans::parse_from_stream( std::cin ) )
+	{
+		for( auto b : mBatch.batches )
+		{
+			for( auto evt : b.events )
+			{
+				spectrum[ evt.channel ]++;
+			}
+		}
+	}
+
+	// Print the spectrum as a JSON array of counts.
+	std::cout << '[';
+	for( uint32_t ch(0); ch <= spectrum.rbegin()->first ; ++ch )
+	{
+		if( 0 != ch ) std::cout << ',';
+		std::cout << spectrum[ch]; // Output 0 in case ch is non-existent.
+	}
+	std::cout << ']';
+
+	return EXIT_SUCCESS;
+}
